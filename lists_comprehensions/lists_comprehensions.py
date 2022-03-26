6ab510426988b60968219fe589dcb60beb9fb553
Santiago Alvarez Fuente <saaf@MacBook-Pro-de-Santiago-2.local>
2022-03-26 18:14:21 +0100

    renamed file, list comprehensions added

---
+++ lists_comprehensions/lists_comprehensions.py
@@ -0,0 +1,8 @@
+# Get squares of a list of numbers
+random_list = [1, 2, 3, 4, 5, 6, 7]
+squares = [n * 2 for n in random_list]
+print(squares)
+
+# Get odd numbers from a list
+odd_numbers = [n for n in random_list if n % 2 != 0]
+print(odd_numbers)
