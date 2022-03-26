98104b15248d20fde4b3f247dd9d3f2c732f3a96
Santiago Alvarez Fuente <saaf@MacBook-Pro-de-Santiago-2.local>
2022-03-26 17:39:38 +0100

    String formatting example added

---
+++ string_formatting/string_formatting.py
@@ -0,0 +1,13 @@
+name = "Luke"
+
+# Option 1 - f strings
+message = f"Hello {name}"
+print(message)
+
+# Option 2 - format
+message = "Hello {}".format(name)
+print(message)
+
+# Option 3 - concat
+message = "Hello " + name
+print(message)
